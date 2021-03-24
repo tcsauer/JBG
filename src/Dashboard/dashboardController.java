ea95686db6c04b0d7e0cd867e1c367b1d92317ff
Traugott Sauer <traugottsauer@Traugotts-Mac-mini.local>
2021-03-24 09:25:11 -0500

    Initial commit

---
+++ src/Dashboard/dashboardController.java
@@ -0,0 +1,23 @@
+package Dashboard;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class dashboardController {
+
+    @FXML
+    private void changeToBook(ActionEvent event) throws IOException {
+        Parent SceneParent = FXMLLoader.load(getClass().getResource("BookMedia.fxml"));
+        Scene newScene = new Scene(SceneParent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newScene);
+        window.show();
+    }
+}
