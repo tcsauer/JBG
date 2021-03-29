9a726afc5b10968dea10ff1b4e48155062a9f51f
mrodri86 <43094250+mrodri86@users.noreply.github.com>
2021-03-28 20:01:52 -0500

    testing commit

--- src/Dashboard/Main.java
+++ src/Dashboard/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
-        primaryStage.setTitle("J B & G Upholstery");
+        primaryStage.setTitle("Testing Phase");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
