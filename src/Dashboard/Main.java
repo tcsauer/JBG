e48afd59a782966525184f282cdfd497237a1868
mrodri86 <43094250+mrodri86@users.noreply.github.com>
2021-03-28 20:13:53 -0500

    checking commit

--- src/Dashboard/Main.java
+++ src/Dashboard/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
         primaryStage.setTitle("Testing Phase");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root, 500, 800));
         primaryStage.show();
     }
 
